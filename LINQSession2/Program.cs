5cd92ada76e15f70cc10a335fb2a69e70f371e47
saramohamed512 <saramohamed12656@gmail.com>
2025-08-30 20:19:39 +0200

    Casting Operators

--- LINQSession2/Program.cs
+++ LINQSession2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Collections;
+using System.Threading;
 
 namespace LINQSession2
 {
@@ -6,7 +7,28 @@ namespace LINQSession2
     {
         static void Main(string[] args)
         {
-           
+            #region Casting Operators
+            #region Get product out of stoc
+            //List<Product> products = ListGenerator.ProductsList.Where(P => P.UnitsInStock == 0).ToList();
+            //Product[] Result = ListGenerator.ProductsList.Where(P => P.UnitsInStock == 0).ToArray();
+            //Dictionary<long, String> Results = ListGenerator.ProductsList.Where(P => P.UnitsInStock == 0).ToDictionary(P=>P.ProductID,P=>P.ProductName);
+            //HashSet<Product> Products = ListGenerator.ProductsList.Where(P => P.UnitsInStock == 0).ToHashSet();
+            //OfType()
+            //ArrayList obj = new ArrayList() { 
+            //    "Ali",
+            //    "sara",
+            //    "mohamed",
+            //    1,
+            //    2, 3,
+            //};
+            //var Result = obj.OfType<int>();
+            //foreach (var product in Result)
+            //{
+            //    Console.WriteLine(product);
+            //}
+            #endregion
+        
+            #endregion
         }
     }
 }
