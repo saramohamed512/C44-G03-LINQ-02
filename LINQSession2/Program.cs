6c35662b323f0ecedbb98020379c74943ae7edff
saramohamed512 <saramohamed12656@gmail.com>
2025-08-30 20:32:05 +0200

    Set Operators

--- LINQSession2/Program.cs
+++ LINQSession2/Program.cs
@@ -34,10 +34,23 @@ namespace LINQSession2
             //Result = Enumerable.Repeat(3, 100);
             //var List=Enumerable.Empty<Product>();
             //List<Product> list = new List<Product>();
-            
+
             //foreach (var result in Result) { 
             //    Console.Write($"{result} ");
-            
+
+            //}
+            #endregion
+            #region Set Operators [Union Family]
+            //var Seq01=Enumerable.Range(0, 100);
+            //var Seq02 = Enumerable.Range(50, 100);
+
+            //var Result= Seq01.Union(Seq02);
+            //Result= Seq01.Concat(Seq02);
+            ////Result = Seq01.Except(Seq02);
+            ////Result= Seq01.Intersect(Seq02);
+            //Result= Result.Distinct();
+            //foreach (var item in Result) { 
+            //Console.Write($"{item} ");
             //}
             #endregion
         }
